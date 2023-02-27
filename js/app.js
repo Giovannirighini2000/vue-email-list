83ac83bb959dca699583705ee6df5891ca09c885
Giovannirighini2000 <righinigiovanni0@gmail.com>
2023-02-27 16:48:55 +0100

    feat added bonus stamp all email

--- js/app.js
+++ js/app.js
@@ -3,23 +3,30 @@ const { createApp } = Vue
 createApp({
 	data() {
 		return {
-			
+
 			emails: [],
+			allEmailsReceived: false,
 		}
 	},
 	methods: {
-		fetchName() {
+		Emailnames() {
 			axios
 				.get('https://flynn.boolean.careers/exercises/api/random/mail')
 				.then((res) => {
 					console.log(res.data.response)
 					this.emails.push(res.data.response)
+					if (this.emails.length === 10) {
+						this.allEmailsReceived = true;
+					}
+
+
 				})
+
 		},
 	},
 	mounted() {
 		for (let i = 0; i < 10; i++) {
-			this.fetchName()
+			this.Emailnames()
 		}
 	},
 }).mount('#app')
